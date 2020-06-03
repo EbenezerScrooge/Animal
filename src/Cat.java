f5c50758c5d36dddf4247e64687c56148563c4d0
ebenezer <3382351283@qq.com>
2020-06-04 01:34:00 +0800

    创建Cat类，实现了reproduce方法

---
+++ src/Cat.java
@@ -0,0 +1,17 @@
+public class Cat implements Animal {
+
+    @Override
+    public void reproduce() {
+        System.out.println("Sexual reproduction");
+    }
+
+    @Override
+    public void grow() {
+
+    }
+
+    @Override
+    public void act() {
+
+    }
+}
